0fe69be63082280667fe92e1106143fa3ab19c3b
Matt Turner <matt@fiverings.uk>
2022-06-18 08:38:13 +0100

    Add connect to device

--- MassiveClock/MainActivity.cs
+++ MassiveClock/MainActivity.cs
@@ -11,12 +11,17 @@ using Google.Android.Material.Snackbar;
 using Android.Content;
 using Android.Bluetooth;
 using System.Collections.Generic;
+using Java.Util;
+using System.Text;
+using System.Linq;
 
 namespace MassiveClock
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Android.Widget.TextView _textviewRawStatus;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,18 +34,40 @@ namespace MassiveClock
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
 
-            //var receiver = new DeviceDiscoveredReceiver(this);
-            // var filter = new IntentFilter(BluetoothDevice.ActionFound);
-            // RegisterReceiver(receiver, filter);
+            var buttonConnect = FindViewById<Android.Widget.Button>(Resource.Id.buttonConnect);
+            buttonConnect.Click += ButtonConnect_Click;
 
-            var list = new List<string>();
-            foreach(var device in BluetoothAdapter.DefaultAdapter.BondedDevices)
+            _textviewRawStatus = FindViewById<Android.Widget.TextView>(Resource.Id.rawStatus);
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            var adapter = BluetoothAdapter.DefaultAdapter;
+            BluetoothDevice device = (from bd in adapter.BondedDevices
+                                      where bd.Name == "MassiveClock"
+                                      select bd).FirstOrDefault();
+
+            if (device == null)
             {
-                list.Add(device.Name);
+                //throw new Exception("Named device not found");
+                var view = (View)sender;
+                Snackbar.Make(view, "Named device not found", Snackbar.LengthLong)
+                    .SetAction("Action", (View.IOnClickListener)null).Show();
             }
-            var listView = FindViewById<Android.Widget.ListView>(Resource.Id.simpleListView);
-            var adapter = new Android.Widget.ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, list);
-            listView.Adapter = adapter;
+
+            var socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
+            socket.Connect();
+
+            var buffer = Encoding.UTF8.GetBytes(">status!");
+            socket.OutputStream.Write(buffer, 0, buffer.Length);
+
+            var statusBuffer = new byte[1024];
+            socket.InputStream.Read(statusBuffer, 0, statusBuffer.Length);
+
+            var status = Encoding.UTF8.GetString(statusBuffer);
+
+            _textviewRawStatus.SetText(status, Android.Widget.TextView.BufferType.Normal);
+            socket.Close();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
